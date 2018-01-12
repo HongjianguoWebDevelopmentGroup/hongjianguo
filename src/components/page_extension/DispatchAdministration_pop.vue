0dffa8d9853bd63e33b1e7a18f4c531cc1a454b7
kevin <princecharmingthi@163.com>
2018-01-12 17:08:06 +0800

    发文管理日期bug

--- src/components/page_extension/DispatchAdministration_pop.vue
+++ src/components/page_extension/DispatchAdministration_pop.vue
@@ -1,48 +1,50 @@
 <template>
   <el-dialog :title="title" :visible.sync="dialogVisible" class="dialog-size" size="small">
-  	<el-form label-width="100px" ref="form" :model="form" :rules="rules">
-  		
-  		<!-- <el-form-item label="发件人" prop="from">
-  			<remote-select type="member" v-model="form.from"></remote-select>
-  		</el-form-item> -->
+    <el-form label-width="100px" ref="form" :model="form" :rules="rules">
+      
+      <!-- <el-form-item label="发件人" prop="from">
+        <remote-select type="member" v-model="form.from"></remote-select>
+      </el-form-item> -->
 
-			<el-form-item label="收件人" prop="to" v-show="type !='confirm'">
-				<remote-select type="member" v-model="form.to"></remote-select>
-			</el-form-item>
+      <el-form-item label="收件人" prop="to" v-show="type !='confirm'">
+        <remote-select type="member" v-model="form.to"></remote-select>
+      </el-form-item>
 
-			<el-form-item label="快递公司" prop="company" v-show="type !='confirm'">
-				<el-autocomplete
+      <el-form-item label="快递公司" prop="company" v-show="type !='confirm'">
+        <el-autocomplete
           v-model="form.company"
           :fetch-suggestions="querySearch"
           placeholder="请输入快递公司名称"
         ></el-autocomplete>
-			</el-form-item>
-
-			<el-form-item label="快递单号" prop="number" v-show="type !='confirm'">
-				<el-input v-model="form.number" placeholder="请填写快递单号"></el-input>
-			</el-form-item>
-
-      <el-form-item label="收文日期" prop="receipt_date" v-show="type != 'add'">
-        <el-date-picker v-model="form.receipt_date" type="date" format="yyyy-MM-dd" placeholder="请选择收件日期" @change="keepSameTime"></el-date-picker>
       </el-form-item>
 
-			<el-form-item label="备注" prop="description" v-show="type !='confirm'">
-        <el-input type="textarea" v-model="form.description" placeholder="请填写备注"></el-input>
+      <el-form-item label="快递单号" prop="number" v-show="type !='confirm'">
+        <el-input v-model="form.number" placeholder="请填写快递单号"></el-input>
       </el-form-item>
 
-      <el-form-item label="发文日期" prop="mail_date" v-show="type !='confirm'">
+      <el-form-item label="收文日期" prop="receipt_date" v-show="type != 'add'" class="is-required">
+        <el-date-picker v-model="form.receipt_date" type="date" placeholder="请选择收件日期"></el-date-picker>
+      </el-form-item>
+
+      <el-form-item label="备注" prop="description">
+        <span v-if="type != 'add'">{{ form.description ? form.description : '暂无备注信息' }}</span>
+        <el-input v-else type="textarea" v-model="form.description" placeholder="请填写备注"></el-input>
+      </el-form-item>
+
+      <el-form-item label="发文日期" prop="mail_date" format="yyyy-MM-dd" v-show="type !='confirm'">
         <el-date-picker v-model="form.mail_date" type="date" placeholder="请选择发文日期"></el-date-picker>
       </el-form-item>
       <el-form-item label="文件清单" prop="projects">
-				<express-list v-model="form.projects" :typeMessage="type"></express-list>
-			</el-form-item>
+        <span v-if="type != 'add' && form.projects.length == 0">暂无文件清单</span>
+        <express-list v-else v-model="form.projects" :typeMessage="type"></express-list>
+      </el-form-item>
 
-			<el-form-item style="margin-bottom: 0px;">
-				<el-button type="primary" @click="add" v-if="type === 'add'" :disabled="btn_disabled">添加</el-button>
-				<el-button type="primary" @click="edit" v-if="type === 'confirm'" :disabled="btn_disabled">确认收到上述文件</el-button>
-			</el-form-item>
+      <el-form-item style="margin-bottom: 0px;">
+        <el-button type="primary" @click="add" v-if="type === 'add'" :disabled="btn_disabled">添加</el-button>
+        <el-button type="primary" @click="confirmFunc" v-if="type === 'confirm'" :disabled="btn_disabled">确认收到上述文件</el-button>
+      </el-form-item>
 
-  	</el-form>
+    </el-form>
   </el-dialog>
 </template>
 
@@ -51,13 +53,12 @@ import AxiosMixins from '@/mixins/axios-mixins'
 import PopMixins from '@/mixins/pop-mixins'
 import RemoteSelect from '@/components/form/RemoteSelect'
 import ExpressList from '@/components/form/ExpressList'
-
 export default {
-  name: 'inventorListPop',
+  name: 'DisatchAdministrationPop',
   mixins: [ AxiosMixins, PopMixins ],
   props:['confirm'],
   data () {
-		return {
+    return {
       dateDisable:false,
       form: {
         to: '',
@@ -68,11 +69,14 @@ export default {
         description: '',
         projects: [],
       },
-      rules: {to: {type: 'number', required: true, message: '收件人必填', trigger: 'change'},
-              company: {required: true, message: '快递公司必填', trigger: 'change'},
-              number: [{ required: true,  message: '快递单号必填', trigger: 'blur' },
-              {pattern: /^[0-9a-zA-Z]*$/, message : '快递单号只能是字母与数字', trigger: 'blur' }],
-              mail_date: { type:'date',required: true, message: '发文日期必填', trigger: 'change'},
+      rules: {
+        to: {type: 'number', required: true, message: '收件人必填', trigger: 'change'},
+        company: {required: true, message: '快递公司必填', trigger: 'change'},
+        number: [
+          { required: true,  message: '快递单号必填', trigger: 'blur' },
+          {pattern: /^[0-9a-zA-Z]*$/, message : '快递单号只能是字母与数字', trigger: 'blur' }
+        ],
+        mail_date: { type:'date',required: true, message: '发文日期必填', trigger: 'change'},
       },
       queryData: [
         { "value": '顺丰'},
@@ -85,7 +89,7 @@ export default {
         { "value": '天天快递'},
         { "value": '宅急送'},
       ],
-		}
+    }
   },
   computed: {
      dateDisabled () {
@@ -105,9 +109,40 @@ export default {
       };
     },
     keepSameTime (val) {
-      console.log(val);
+      // console.log(typeof(new Date(val)));
       this.form.receipt_date = val;
+      console.log(val)
     },
+    confirmFunc () {
+      if( !this.form.receipt_date ) { return this.$message({type: 'warning', message: '请选择收文日期'});  }
+
+      const url = '/api/expresses';
+      const data = this.form;
+      const success = _=>{
+        this.$message({message: '确认收文成功', type: 'success'});
+        this.dialogVisible = false;
+        this.$emit('refresh', 'edit');
+      }
+      const complete = _=>{
+        this.btn_disabled = false;
+      }
+
+
+      this.$refs.form.validate(_=>{
+         this.$axiosPost({url, data, success, complete})
+      })
+    },
+    setForm (data) {
+      for(let key in this.form) {
+        const d = data[key];
+        if(d == undefined) continue;
+        if(key == 'mail_date') {
+          this.form[key] = new Date(d);
+        }else {
+          this.form[key] = d;
+        }
+      }
+    }
   },
   components: {  ExpressList, RemoteSelect  },
   URL: '/api/expresses',
--- src/mixins/pop-mixins.js
+++ src/mixins/pop-mixins.js
@@ -1,47 +1,45 @@
 export default {
-	data () {
-		return {
-			id: '',
+  data () {
+    return {
+      id: '',
       type: '',
       btn_disabled: false,
-		  dialogVisible: false,
-		}
-	},
-	computed: {
-		title () {
-			const t = this.type;
-			const tex = this.$options.REMINDER_TEXT;
+      dialogVisible: false,
+    }
+  },
+  computed: {
+    title () {
+      const t = this.type;
+      const tex = this.$options.REMINDER_TEXT;
 
-			return t == 'add' ? `新增${tex}` : t == 'confirm'? `确认收文` : `编辑${tex}`;
-		}
-	},
-	methods: {
-		show ( type='add', data ) {
-  		this.type = type;
+      return t == 'add' ? `新增${tex}` : t == 'confirm'? `确认收文` : `编辑${tex}`;
+    }
+  },
+  methods: {
+    show ( type='add', data ) {
+      this.type = type;
       this.dialogVisible = true;
-     	this.$nextTick(_=>{
-     		console.log(this.type);
+      this.$nextTick(_=>{
         this.$refs.form.resetFields();  
-      	
-	      if(type === 'edit'|| type === 'confirm') {
+        
+        if(type === 'edit' || type === 'confirm') {
           if(data instanceof Object) {
             const copy = this.$tool.deepCopy(data);
             this.id = copy.id; 
             this.setForm ? this.setForm(data) : this.$tool.coverObj(this.form, copy);  
           }else if(data instanceof String || data instanceof Number) {
             data -= 0;
-          }	        
-	      }
-     	});
-  	},
+          }         
+        }
+      });
+    },
     confirmCallback () {
       this.$message({message:'已确认收文',type:'success'});
       this.dialogVisible = false;
     },
-  	add () {
+    add () {
       const url = this.$options.URL;
       const tex = this.$options.REMINDER_TEXT;
-      
       const data = this.addForm 
                     ? this.addForm() : this.submitForm 
                       ? this.submitForm() : this.form;
@@ -64,13 +62,11 @@ export default {
     edit () {
       const url = `${this.$options.URL}/${this.id}`;
       const tex = this.$options.REMINDER_TEXT;
-      
       const data = this.editForm 
                     ? this.editForm() : this.submitForm 
                       ? this.submitForm() : this.form;
-      
       const success = _=>{
-        this.$message({message: this.type == 'confirm'? `确认收文成功` : `编辑${tex}成功`, type: 'success'});
+        this.$message({message: `编辑${tex}成功`, type: 'success'});
         this.dialogVisible = false;
         this.$emit('refresh', 'edit');
       }
@@ -79,24 +75,11 @@ export default {
       }
 
       this.$refs.form.validate(_=>{
-         // console.log(data);
-        if(data.hasOwnProperty('receipt_date')) {
-           if(!data.receipt_date) {
-            return this.$message({type: 'warning',message: '收文日期不能为空!'});
-          }else {
-            if(_) {
-            this.btn_disabled = true;
-            this.$axiosPut({url, data, success, complete});        
-            }
-          }
-        }else {
-          if(_) {
-            this.btn_disabled = true;
-            this.$axiosPut({url, data, success, complete});        
-            }
+        if(_) {
+          this.btn_disabled = true;
+          this.$axiosPut({url, data, success, complete});        
         }  
       })
-
     }
-	}
+  }
 }
