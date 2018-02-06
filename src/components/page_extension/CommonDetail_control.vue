ea23709d02905d88011831d11503185f94e9cfb1
kevin <princecharmingthi@163.com>
2018-02-06 15:37:20 +0800

    附件下载

--- src/components/page_extension/CommonDetail_control.vue
+++ src/components/page_extension/CommonDetail_control.vue
@@ -1,99 +1,159 @@
 <template>
   <div class="main">
-  	<app-table :columns="columns" :data="tableData">
-			<template slot="row_action" slot-scope="scope">
-				<el-button type="text" size='mini' @click='toggle(scope.row)'>{{ show == scope.row.id ? '隐藏任务详情' : '显示任务详情'}}</el-button>
-			</template>
-  	</app-table>
-  	<template v-if="show ? true : false">
-  		<app-table :columns="columns2" :data="tableData2" style="margin-top: 20px;"></app-table>
-  	</template>
+    <table-component :tableOption="option" :data="tableData">
+    <template slot="row_action" slot-scope="scope">
+      <el-button type="text" size='mini' @click='toggle(scope.row)'>{{ show == scope.row.id ? '隐藏任务详情' : '显示任务详情'}}</el-button>
+    </template>
+    </table-component>
+    <template v-if="show ? true : false">
+      <table-component :tableOption="option2" :data="tableData2"></table-component>
+    </template>
+    <el-dialog title="附件下载" :visible.sync="dialogVisible" :modal="false">
+      <template>
+        <table-component :tableOption="option3" :data="tableData3.attachments"></table-component>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import AppTable from '@/components/common/AppTable'
 import TableComponent from '@/components/common/TableComponent'
 import AxiosMixins from '@/mixins/axios-mixins'
 
 export default {
-  name: 'commonDetailControl',
+  name: 'hello',
   mixins: [ AxiosMixins ],
   data () {
-		return {
-	  	columns: [
-	  		{ type: 'text', label: '管制事项', prop: 'name' },
-	  		{ type: 'text', label: '当前节点', prop: 'node_name' },
-	  		{ type: 'text', label: '承办人', prop: 'person_in_charge_name' },
-	  		{ type: 'text', label: '开始时间', prop: 'start_time' },
-	  		{ type: 'text', label: '指定期限', prop: 'due_time' },
-	  		{ type: 'text', label: '代理机构', prop: 'agency' },
-	  		{ type: 'text', label: '代理人', prop: 'agent' },
-	  		{ 
-	  			type: 'action', 
-	  			label: '操作',
-	  			btns_render: true,
-	  		},
-	  	],
-		  columns2: [
-	  	 	{ type: 'text', label: '子任务名称', prop: 'flow_node' },
-	  	 	{ type: 'text', label: '开始时间', prop: 'start_time' },
-	  	 	{ type: 'text', label: '完成时间', prop: 'end_time' },
-	  	 	{ type: 'text', label: '承办人', prop: 'person_in_charge_name' },
-	  	 	{ 
-	  	 		type: 'text', label: '附件', prop: 'attachments',
-	  	 		render (h,item) {
-	  	 		
-	  	 			return h(
-	  	 				'span', 
-	  	 				item.map(function (g) {
-    							return h('a', 
-    							{
-    								style: {
-								    	marginRight: '2px',
-										},
-										attrs: {
-											href: g.downloadUrl,
-										},
-    							},g.name)
-  						})
-  					)
-	  	 		} 
-	  	 	},
-	  	 	{ type: 'text', label: '备注', prop: 'remark' },
-		  ],
-		  tableData2: [],
-		  show: null,
-		};
+  return {
+      option: {
+        'is_search': false,
+        'is_pagination': false,
+        'columns': [
+          { type: 'text', label: '管制事项', prop: 'name' },
+          { type: 'text', label: '当前节点', prop: 'node_name' },
+          { type: 'text', label: '承办人', prop: 'person_in_charge_name' },
+          { type: 'text', label: '开始时间', prop: 'start_time' },
+          { type: 'text', label: '指定期限', prop: 'due_time' },
+          { type: 'text', label: '代理机构', prop: 'agency' },
+          { type: 'text', label: '代理人', prop: 'agent' },
+          { 
+            type: 'action', 
+            label: '操作',
+            btns_render: 'action'
+          },
+        ]
+      },
+      option2: {
+        'is_search': false,
+        'is_pagination': false,
+        'rowClick': this.handleRowClick,
+        'columns': [
+          { type: 'text', label: '子任务名称', prop: 'flow_node' },
+          { type: 'text', label: '开始时间', prop: 'start_time' },
+          { type: 'text', label: '完成时间', prop: 'end_time' },
+          { type: 'text', label: '承办人', prop: 'person_in_charge_name' },
+          { 
+            type: 'text', label: '附件', prop: 'attachments',
+            render (h,item) {
+            
+              return h(
+                'span', 
+                item.map(function (g) {
+                    return h('a', 
+                    {
+                      style: {
+                      marginRight: '2px',
+                  },
+                  attrs: {
+                    href: '#',
+                  },
+                    },g.name)
+                })
+              )
+            } 
+          },
+          { type: 'text', label: '备注', prop: 'remark' },
+          // { 
+          //  type: 'text', label: '附件', prop: 'attachments',
+          //  render (h, item) {
+          //    return h('a', item.name);
+          //  }
+          // },
+          // { 
+          //  type: 'action', 
+          //  label: '操作',
+          //  btns: [{
+          //    text: '完成任务',
+          //    btn_type: 'text',
+          //    click: ()=>{alert("完成任务")},
+          //  }, {
+          //    text: '任务转出',
+          //    btn_type: 'text',
+          //    click: ()=>{alert("任务转出")},
+          //  }]
+          // },
+        ]
+      },
+      option3: {
+        'is_search': false,
+        'is_pagination': false,
+        'columns':[
+          { type: 'text', label: '文件名称', prop: 'name'},
+          { type: 'text', label: '上传日期', prop: 'end_time'},
+          { type: 'text', label: '上传人', prop: 'person_in_charge_name'},
+          { type: 'text', label: '大小', prop: 'size'},
+          { 
+            type: 'action',
+            label: '操作',
+            min_width: '120',
+            btns: [
+              { type: 'view', click: ({viewUrl})=>{window.open(viewUrl)}},
+                  { type: 'download', click: ({downloadUrl})=>{window.open(downloadUrl)}},  
+            ]
+          }
+        ],
+      },
+      tableData2: [],
+      tableData3: [],
+      show: null,
+      dialogVisible: false,
+    };
   },
   methods: {
-  	toggle ({id}) {
-  		if(this.show == null || this.show != id) {
-  			const url = `/api/tasks/${id}`;
-  			const success = _=>{
-  				this.tableData2 = _.task.siblings;
-  				// console.log(_)
-  			};
+    toggle ({id}) {
+      // console.log(this.tableData[0].id)
+      // console.log(id);
+      if(this.show == null || this.show != id) {
+        const url = `/api/tasks/${id}`;
+        const success = _=>{
+          this.tableData2 = _.task.siblings;
+          // console.log(_)
+        };
 
-  			this.axiosGet({url, success});	
-  		}
-
-  		this.show = this.show == id ? null : id;
-  	}
+        this.axiosGet({url, success});  
+      }
+      this.show = this.show == id ? null : id;
+    },
+    handleRowClick (row) {
+      console.log(row);
+      this.tableData3 = row;
+      this.dialogVisible = true;
+    },
   },
   computed: {
-  	tableData () {
-  		return this.$store.getters.detailTasks;
-  	}
+    tableData () {
+      return this.$store.getters.detailTasks;
+    }
   },
   watch: {
-  	tableData () {
-  		this.show = null;
-  	}
+    tableData (val) {
+      if(val.length != 0) {
+        this.toggle(val[0]);  
+      }
+      
+    },
   },
-  components: { 
-  	AppTable, 
-  },
+  components: { TableComponent },
 }
 </script>
 
