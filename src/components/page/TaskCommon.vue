b2135300381536000397056760af45ae816323bc
kevin <princecharmingthi@163.com>
2018-06-11 15:56:55 +0800

    修复task undefined

--- src/components/page/TaskCommon.vue
+++ src/components/page/TaskCommon.vue
@@ -84,7 +84,7 @@
       </span>
       <span slot="header" style="float: right">
         <el-button size="small" type="primary" @click="dialogEditVisible = true" v-if="menusMap && !menusMap.get('/tasks/update')">编辑</el-button>
-        <el-button size="small" style="margin-left: 0px;" v-if="menusMap && !menusMap.get('/tasks/transfer')" @click="dialogTranserVisible = true; transfer_person = {id: currentRow.person_in_charge, name: currentRow.person_in_charge_name }">移交</el-button>
+        <el-button size="small" style="margin-left: 0px;" v-if="menusMap && !menusMap.get('/tasks/transfer')" @click="dialogTranserVisible = true; transfer_person = {id: currentRow.person_in_charge.id, name: currentRow.person_in_charge.name }">移交</el-button>
         <el-button size="small" @click="handleReject" style="margin-left: 0px;" v-if="menusMap && !menusMap.get('/tasks/reject')">退回</el-button>
       </span>
       <el-tabs v-model="activeName">        
